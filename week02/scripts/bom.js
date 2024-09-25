712d01a7edecbcc3b649bb29054535df28f157b2
Eldon <Birdheldon@gmail.com>
2024-09-25 13:40:28 -0600

    Started on the next assignmnet

---
+++ week02/scripts/bom.js
@@ -0,0 +1,19 @@
+const input = document.querySelector('#favchap');
+const button = document.querySelector('button');
+const list = document.querySelector("list");
+
+const li = document.createElement('li');
+const deleteButton = document.createElement('button');
+
+li.textContent = input.value;
+
+deleteButton.textContent = '❌';
+
+li.append(deleteButton);
+li.append(li);
+
+button.addEventListener('click', function(){
+    if (input.value.trim() != ""){
+        
+    }
+});
