4a2e88d150fd6e33f34a12fa90289e754ee13fc7
Eldon <Birdheldon@gmail.com>
2024-09-25 22:26:59 -0600

    HAMBURGER!!!

--- scripts/temples.js
+++ scripts/temples.js
@@ -6,3 +6,14 @@ const today = new Date();
 lastModified.innerHTML = `last modified: <span class="highlight">${document.lastModified}</span>`;
 
 copyright.innerHTML = `&copy <span class="highlight">${today.getFullYear()}</span> Hunter Bird`;
+
+
+
+const hamButton = document.querySelector('#menu');
+const navigation = document.querySelector('.navigation');
+
+hamButton.addEventListener('click', () => {
+
+    navigation.classList.toggle('open');
+    hamButton.classList.toggle('open');
+});
