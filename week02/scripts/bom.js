1a45da8434f3d89b534503ffb5341221d5327450
huntereldonbird <56803340+EldonBird@users.noreply.github.com>
2024-09-25 19:12:38 -0600

    somethuaiunawd

--- week02/scripts/bom.js
+++ week02/scripts/bom.js
@@ -1,19 +1,23 @@
 const input = document.querySelector('#favchap');
-const button = document.querySelector('button');
-const list = document.querySelector("list");
+const button  = document.querySelector('button');
+const list = document.querySelector('ul')
 
-const li = document.createElement('li');
-const deleteButton = document.createElement('button');
 
-li.textContent = input.value;
-
-deleteButton.textContent = '❌';
-
-li.append(deleteButton);
-li.append(li);
 
 button.addEventListener('click', function(){
-    if (input.value.trim() != ""){
-        
+    if(input.value.trim() != ''){
+        const li = document.createElement('li');
+        const deleteButton = document.createElement('button');
+        li.textContent = input.value;
+        deleteButton.textContent = '❌';
+        list.append(li, deleteButton);
+        input.value ='';
     }
+
+});
+
+deleteButton.addEventListener('click', function(){
+    list.removeChild(li, deleteButton);
+    input.value = '';
+    input.focus();
 });
