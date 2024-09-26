983c2b30ab7700e1018ef7cf7aa29cb7896e9097
Eldon <Birdheldon@gmail.com>
2024-09-25 21:58:47 -0600

    awdawdawd

---
+++ scripts/temples.js
@@ -0,0 +1,8 @@
+const lastModified = document.querySelector("#lastModified");
+const copyright = document.querySelector("#copyright");
+
+const today = new Date();
+
+lastModified.innerHTML = `last modified: <span class="highlight">${document.lastModified}</span>`;
+
+copyright.innerHTML = `&copy <span class="highlight">${today.getFullYear()}</span> Hunter Bird`;
