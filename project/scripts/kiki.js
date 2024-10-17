d54d0792d39ff9607a1812b906e567b4837259cf
EldonBird <Birdheldon@gmail.com>
2024-10-17 12:37:28 -0600

    updated programs code, close to completion if you make a way for it to read stuff

--- project/scripts/kiki.js
+++ project/scripts/kiki.js
@@ -11,7 +11,7 @@ copyright.innerHTML = `&copy <span class="highlight">${today.getFullYear()}</spa
 
 
 let pCard = document.createElement("section");
-let title = document.createElement("h2");
+let title = document.createElement("h1");
 let firstBreak = document.createElement("hr");
 let secondBreak = document.createElement("hr");
 
@@ -19,11 +19,13 @@ let articlebutton = document.createElement("button");
 let gallerybutton = document.createElement("button");
 let gitbutton = document.createElement("button");
 
+let articlecontainer = document.createElement("div");
+
 let articletext = document.createElement("p");
 let projectphoto = document.createElement("img");
 
 title.textContent = "Palimpsest";
-articletext.textContent = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris dolor tellus, tempus vitae sollicitudin ac, imperdiet eu dolor. Ut sagittis ut augue a tristique. Sed rhoncus est eget ligula tempor posuere. Nam tincidunt posuere nisi ornare porta. Maecenas lobortis lacinia diam sit amet dictum. Curabitur sodales purus libero, at aliquet purus feugiat vel. Suspendisse et interdum nisi, vitae iaculis lacus. Vivamus ipsum velit, consectetur nec ipsum quis, lacinia finibus nulla. Vivamus volutpat purus eget ipsum lobortis mollis. Nullam tincidunt lectus felis, non blandit arcu rhoncus ac.";
+articletext.textContent = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed pellentesque dignissim blandit. Curabitur odio velit, condimentum id nisl in, consectetur varius turpis. Aliquam ac purus nec urna elementum pulvinar. Integer ut venenatis lectus. Nulla fringilla arcu convallis ultricies dictum. Duis ut bibendum metus, quis consectetur ligula. Nullam porta leo at luctus tincidunt. Nulla quis massa sed mi sagittis facilisis. Praesent nulla elit, tempus id interdum ullamcorper, scelerisque nec ante. Integer volutpat mauris a cursus rutrum. Suspendisse hendrerit, purus ornare commodo iaculis, nibh elit efficitur diam, a interdum ex mi a dui. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Sed cursus urna et nulla sodales consequat. Vestibulum viverra leo vitae euismod tincidunt. Suspendisse ultricies, metus quis posuere rhoncus, tortor odio ullamcorper nisl, nec finibus lacus sapien vel risus. Integer aliquam ipsum quis pretium euismod. Nam consequat tortor turpis, ac facilisis nulla ornare ut. In hac habitasse platea dictumst. Aliquam porttitor, lacus eget bibendum vestibulum, ligula tellus bibendum nisl, eu sagittis nunc augue a arcu. Praesent ornare eros massa, nec pharetra dolor viverra non. Curabitur non rutrum nibh, ac feugiat purus. Vivamus malesuada elit sit amet mauris volutpat sodales. Nam porttitor dui ac elit pulvinar egestas. Nullam augue sem, cursus a quam et, finibus luctus felis. Nunc vitae ornare urna. Pellentesque tempor elit dignissim est pharetra, sit amet placerat elit maximus. Morbi at sem rhoncus, imperdiet eros ut, imperdiet mauris. Sed varius, urna sit amet tristique mattis, nisl augue scelerisque lectus, sit amet maximus diam enim et velit. Curabitur porta mi leo, a porttitor mauris aliquam sit amet. Ut porttitor libero nisl, eget dignissim ipsum viverra a. Ut in lacus ac turpis finibus aliquet. Ut leo mi, fringilla et eleifend non, dictum quis nisl. Mauris non libero a erat ornare lobortis. Mauris dapibus id enim ac varius. Maecenas et pretium metus. Curabitur dictum commodo dolor, quis accumsan ante auctor vel. Nam quis mauris sit amet odio scelerisque ornare eu non mauris. Nulla dui dui, facilisis quis aliquam sed, porttitor eget erat. Morbi varius mi varius est rutrum, eget efficitur urna semper. Maecenas aliquet vitae est elementum placerat. Phasellus quis rhoncus mauris. Vestibulum volutpat egestas augue vitae laoreet. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Mauris turpis massa, lacinia id urna sit amet, viverra pretium lacus. Quisque nec porta mauris. Duis posuere nibh finibus est consectetur, ut aliquam quam finibus. Sed molestie purus vitae magna tincidunt, eget placerat est vestibulum. Nam at pretium diam. Curabitur sit amet venenatis quam. Nullam condimentum augue purus, tincidunt ullamcorper diam maximus at.";
 
 articlebutton.textContent = "Article";
 gallerybutton.textContent = "gallery";
@@ -33,11 +35,11 @@ articlebutton.setAttribute("class", "articlebuttons");
 gallerybutton.setAttribute("class", "articlebuttons");
 gitbutton.setAttribute("class", "articlebuttons");
 
-
-
-
-
-
+articlecontainer.setAttribute("class", "articlecontainers");
+articletext.setAttribute("class", "articleParagraphs");
+projectphoto.setAttribute("class", "articeimgage");
+projectphoto.setAttribute("src", "images/tmep.jpg")
+title.setAttribute("class", "articleTitle");
 
 
 pCard.appendChild(title);
@@ -46,8 +48,12 @@ pCard.appendChild(articlebutton);
 pCard.appendChild(gallerybutton);
 pCard.appendChild(gitbutton);
 pCard.appendChild(secondBreak);
-pCard.appendChild(articletext);
-pCard.appendChild(projectphoto);
+
+
+pCard.appendChild(articlecontainer);
+
+articlecontainer.appendChild(articletext);
+articlecontainer.appendChild(projectphoto);
 
 
 
